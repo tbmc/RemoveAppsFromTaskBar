4b1354d552546a49b76576838708392011c9edcf
Thibault BALLIER <thibault.ballier@axa.fr>
2019-05-15 11:08:26 +0200

    It is working

---
+++ RemoveShittyAppsFromTaskbar/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.IO;
+
+
+namespace RemoveShittyAppsFromTaskbar
+{
+    class Program
+    {
+        public static List<string> ReadFileLineByLine(string path)
+        {
+            List<string> output = new List<string>();
+            string line;
+            var file = new System.IO.StreamReader(path);
+            while((line = file.ReadLine()) != null)
+            {
+                if(line.Length > 1)
+                {
+                    output.Add(line);
+                }
+            }
+            file.Close();
+            return output;
+        }
+
+        static int Main(string[] args)
+        {
+            string fileName = "file_list.txt";
+            if(args.Length >= 1)
+            {
+                fileName = args[0];
+            }
+
+            if (!File.Exists(fileName))
+            {
+                Console.WriteLine($"File {fileName} not found");
+            }
+
+            var list = ReadFileLineByLine(fileName);
+            bool success = RemoveFromTaskBar.RemoveListFromTaskbar(list);
+
+            Console.WriteLine($"L'opération s'est bien déroulé : {success}");
+            Thread.Sleep(5000);
+            return success ? 0 : 1;
+        }
+    }
+}
---
+++ RemoveShittyAppsFromTaskbar/RemoveFromTaskBar.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveShittyAppsFromTaskbar
+{
+    class RemoveFromTaskBar
+    {
+
+        public static bool RemoveListFromTaskbar(List<string> list)
+        {
+            bool success;
+            try
+            {
+                Utils.ChangeImagePathName("explorer.exe");
+                success = RemoveListFromTaskBarInternal(list);
+            }
+            finally
+            {
+                Utils.RestoreImagePathName();
+            }
+            return success;
+        }
+
+        private static bool RemoveListFromTaskBarInternal(List<string> list)
+        {
+            bool sucess = true;
+            foreach(string fileName in list)
+            {
+                bool tempSuccess = Utils.PinUnpinTaskbar(fileName, false);
+                if (!tempSuccess)
+                    sucess = false;
+            }
+            return sucess;
+        }
+
+    }
+}
